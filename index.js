a2203c6b53ef8e01bee624f2213e90335b1faa2b
ajay-0470 <ajay041822@gmail.com>
2024-05-27 17:24:20 +0530

    almost there

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 console.log('Namaste');
+const option=["rock", "paper", "scissors"];
+function getcomputerchoice()
+{
+    const choice=option[Math.floor(Math.random() * option.length)];
+    console.log(choice);
+    return choice;
+    
+}
+function checkwinner(playerselection, computerselection)
+{
+    if (playerselection==computerselection){
+         return "Tie";
+        }
+        else if(
+            (playerselection=="rock"&&computerselection=="scissors")||(playerselection="scissors"&&computerselection=="paper")||(playerselection=="paper"&&computerselection=="rock")
+        )
+{
+    return "Player";
+}
+
+    else {
+        return "Computer";
+    }
+}
+    
+function playround(playerselection, computerselection){
+    const result=checkwinner(playerselection,computerselection);
+    if (result=="Tie")
+        {
+            return "It's a Tie!";
+        }
+            else if(result=="Player")
+                {
+                    console.log("You Win!!     "+playerselection+ "   beats   "+computerselection);
+                }
+                else if(result=="Computer")
+                    {
+                        console.log("You Loose!     " +computerselection+ "    beats    "+playerselection);
+                    }
+                
+        }
+
+
+const playerselection="rock";
+const computerselection=getcomputerchoice();
+console.log(playround(playerselection,computerselection));
